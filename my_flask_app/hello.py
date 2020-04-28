95b1b9124131bf08b75a9d78b4a02f37ddcd58fd
Matiwos Birbo <workubirbo@gmail.com>
2020-04-28 12:53:14 -0500

    made change

--- my_flask_app/hello.py
+++ my_flask_app/hello.py
@@ -1,6 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return 'Hello World! My name is MMMMM'
+    return "Hello, World!!!"
+
+#@app.route("/david")
+#def david():
+    #return "Hello, David!"
+
+@app.route("/<string:name>")
+def hello(name):
+    name = name.capitalize()
+    return f"<h1> Hello, {name} </h1>!"
